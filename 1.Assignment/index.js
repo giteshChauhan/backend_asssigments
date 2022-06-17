e8a24d4b63011472ded26f770418d2e3f871108f
Gitesh Chauhan <giteshchauhan20@gmail.com>
2022-06-17 20:34:24 +0530

    added wallet.js route

--- 1.Assignment/index.js
+++ 1.Assignment/index.js
@@ -4,11 +4,13 @@ const express = require('express');
 const app = express();
 require('express-async-errors');
 const user = require('./routes/user');
-const login = require('./routes/login');
+const wallet = require('./routes/wallet');
+const auth = require('./middleware/auth');
 
+app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use('/api/user',user);
-app.use('/api/login',login);
+app.use('/api/wallet',auth,wallet);
 
 mongoose.connect(config.get('db'))
     .then(() => console.log('Database connected successfully.'))
