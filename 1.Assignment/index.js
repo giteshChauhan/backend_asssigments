6466621eef27a25be381a4befea28b5d5ad2961f
Gitesh Chauhan <giteshchauhan20@gmail.com>
2022-06-18 21:39:48 +0530

    added transaction.js route

--- 1.Assignment/index.js
+++ 1.Assignment/index.js
@@ -5,12 +5,14 @@ const app = express();
 require('express-async-errors');
 const user = require('./routes/user');
 const wallet = require('./routes/wallet');
+const transaction = require('./routes/transaction');
 const auth = require('./middleware/auth');
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use('/api/user',user);
 app.use('/api/wallet',auth,wallet);
+app.use('/api/transaction',auth,transaction);
 
 mongoose.connect(config.get('db'))
     .then(() => console.log('Database connected successfully.'))
